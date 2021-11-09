c7f3f9837b1351a6046e8fc66420743b672bfa83
guna-rakesh1436 <93518553+guna-rakesh1436@users.noreply.github.com>
2021-11-09 13:02:05 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+# fibonacci nubers upto 50 
+p,q=0,1
+while q<50:
+    print(q)
+    p,q = q,p+q
+
+
+# In[12]:
+
+
+print("end")
+
+
+# In[ ]:
+
+
+
+
