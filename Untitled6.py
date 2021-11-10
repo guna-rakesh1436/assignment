b34b2e2d764a0033b8dc0a0d4d530347746a70b4
guna-rakesh1436 <93518553+guna-rakesh1436@users.noreply.github.com>
2021-11-10 12:16:10 +0530

    Add files via upload

---
+++ Untitled6.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+numbers = (1,2,3,4,5,6,7,8,9)
+odd = 0 
+even = 0
+for x in numbers:
+    if not x % 2:
+        even+=1
+else:
+        odd+=1
+        print("the number of odd numbers:",odd)
+        print("the number of even numbers:",even)
+    
+
+
+# In[ ]:
+
+
+
+
