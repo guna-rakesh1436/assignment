a5a0c13f8d4f32d11cf00af3cfbd0d0094098f1b
guna-rakesh1436 <93518553+guna-rakesh1436@users.noreply.github.com>
2021-11-22 00:01:23 +0530

    Add files via upload

---
+++ Untitled10.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sum(number):
+    total = 0
+    for x in number:
+        total += x
+    return total
+print(sum((8, 2, 3, 0, 7)))
+
+
+# In[ ]:
+
+
+
+
